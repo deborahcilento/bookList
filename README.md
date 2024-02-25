3af6abb389b9d8c7617c09065080c65db3fd6928
deborah <debbiecilento@gmail.com>
2024-02-24 21:52:13 -0700

    all files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bookList
+
+This is a web component that displays book information in a nice card layout. It uses the book-card component.   
+
+Features  
+- Displays book info in a clean card format  
+- Custom element web component for easy reuse  
+- Attributes for customizing data  
+- Basic styling included  
+
+Future Enhancements  
+- Allow passing in image for book cover  
+- Add option for link to purchase book  
---
+++ books.js
@@ -0,0 +1,59 @@
+// Create a class for the element
+class Book extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const title    = this.getAttribute('title');
+        const subtitle = this.getAttribute('subtitle');
+        const author   = this.getAttribute('author');
+        const publisher = this.getAttribute('publisher');
+        const description = this.getAttribute('description');
+        const pages = this.getAttribute('pages');
+        const website = this.getAttribute('website');
+        this.innerHTML = `
+            <div class="card">
+              <h5 class="card-header">${title}</h5>
+              <div class="card-body">
+                <h5>${subtitle}</h5>
+                    <p class="card-text">
+                    <table class="table">
+                        <tr>
+                            <td class="text-primary font-weight-bold">Title:</td>
+                            <td>${title}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-primary font-weight-bold">Subtitle:</td>
+                            <td>${subtitle}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-primary font-weight-bold">Author:</td>
+                            <td>${author}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-primary font-weight-bold">Publisher:</td>
+                            <td>${publisher}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-primary font-weight-bold">Description:</td>
+                            <td>${description}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-primary font-weight-bold">Pages:</td>
+                            <td>${pages}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-primary font-weight-bold">Website:</td>
+                            <td>${website}</td>
+                        </tr>
+                    </table>
+                    </p>
+              </div>
+            </div>
+        `;  
+    }
+}
+
+// Define the new element
+customElements.define('mit-book', Book);
